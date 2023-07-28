cfe2c90764dda673a8767130d301de00397f1486
ZenalArifin <39817707+z3n70@users.noreply.github.com>
2023-07-28 14:13:48 +0700

    delete comment proc_get_status.php

--- proc_get_status.php
+++ proc_get_status.php
@@ -8,16 +8,14 @@ $descriptorspec = array(
 $process = proc_open('id', $descriptorspec, $pipes);
 
 if (is_resource($process)) {
-    // Dapatkan informasi status proses
+
     $status = proc_get_status($process);
 
-    // Cetak informasi status
     echo "PID: " . $status['pid'] . PHP_EOL;
     echo "Command: " . $status['command'] . PHP_EOL;
     echo "Running: " . ($status['running'] ? 'Yes' : 'No') . PHP_EOL;
     echo "Exit Code: " . $status['exitcode'] . PHP_EOL;
 
-    // Tutup proses dan pipa
     fclose($pipes[0]);
     fclose($pipes[1]);
     fclose($pipes[2]);
